789cf18cfb09a5ce0c8298f522cb76e41eda98a7
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2021-05-08 12:55:24 +0200

    generalized database naming

--- mysql.php
+++ mysql.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 
 try {
-  $mysql = new PDO("mysql:host=$servername;dbname=general", $username, $password);
+  $mysql = new PDO("mysql:host=$servername;port=8111;dbname=wallet_website", $username, $password);
   // set the PDO error mode to exception
   $mysql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
