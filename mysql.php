c2b50e46aadff6b397d93dd00714a5fd64d6ef1f
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2021-05-21 14:59:08 +0200

    removed unnecessary files

--- mysql.php
+++ mysql.php
@@ -2,9 +2,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$db = "dbs2000829";
 
 try {
-  $mysql = new PDO("mysql:host=$servername;port=8111;dbname=wallet_website", $username, $password);
+  $mysql = new PDO("mysql:host=$servername;port=8111;dbname=wallet_website", $username, $password, $db);
   // set the PDO error mode to exception
   $mysql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
